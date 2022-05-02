546ec9472246af5e42d26cd4f92481109449d171
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-05-02 14:20:51 +0200

    length stampata

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Descrizione:
+Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall'utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+ */
+$paragrafo = "lorenzo sta iniziando a usare php!";
+$lunghezza =   strlen($paragrafo);
+?>
+
+<h1>
+    la porzione di testo scelta è:
+    <?php
+    echo $paragrafo
+    ?>
+</h1>
+
+<p>
+    la sua lunghezza è
+    <?php
+    echo $lunghezza
+    ?>
+</p>
