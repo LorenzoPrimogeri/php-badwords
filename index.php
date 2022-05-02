f0a26adc040e94eecd3a12e2b2e32aa6f7897346
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-05-02 14:39:20 +0200

    exerrcise complete

--- index.php
+++ index.php
@@ -4,10 +4,13 @@ Descrizione:
 Creare una variabile con un paragrafo di testo a vostra scelta.
 Stampare a schermo il paragrafo e la sua lunghezza.
 Una parola da censurare viene passata dall'utente tramite parametro GET.
-Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+Stampare di nuovo il paragrafo e la sua lunghezza,
+dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
  */
-$paragrafo = "lorenzo sta iniziando a usare php!";
+$paragrafo = "lorenzo sta iniziando a usare php!, lorenzo sta iniziando a usare php!, lorenzo sta iniziando a usare php!, lorenzo sta iniziando a usare php!";
 $lunghezza =   strlen($paragrafo);
+$occorrenza = $_GET["occorrenze"];
+
 ?>
 
 <h1>
@@ -22,4 +25,15 @@ $lunghezza =   strlen($paragrafo);
     <?php
     echo $lunghezza
     ?>
+</p>
+
+<?php
+$sostituzione = str_replace($occorrenza, "***", $paragrafo)
+?>
+<p>
+    l'la stringa rimuovendo l'occorenza è:
+    <?php
+    echo $sostituzione
+    ?>
+
 </p>
